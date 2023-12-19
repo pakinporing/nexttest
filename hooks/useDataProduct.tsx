fd022987d67ec0d712cbe4bc9c30521e03e125fd
“pakinporing” <pakinporing@gmail.com>
2023-12-19 09:46:23 +0700

    test

--- hooks/useDataProduct.tsx
+++ hooks/useDataProduct.tsx
@@ -1,6 +1,6 @@
-import { useContext } from 'react';
-import { DataProductContext } from '../contexts/DataProductContext';
+// import { useContext } from 'react';
+// import { DataProductContext } from '../contexts/DataProductContext';
 
-export default function useDataProduct() {
-  return useContext(DataProductContext);
-}
+// export default function useDataProduct() {
+//   return useContext(DataProductContext);
+// }
